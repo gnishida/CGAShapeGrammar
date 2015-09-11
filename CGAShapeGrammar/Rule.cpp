61096d8ae2813ff1d68f7b472fcc09020aad78a5
gnishida <gnishida@purdue.edu>
2015-09-11 14:47:36 -0400

    fix a bug in decodeSplitSizes.

--- CGAShapeGrammar/Rule.cpp
+++ CGAShapeGrammar/Rule.cpp
@@ -1,4 +1,4 @@
-#include "Rule.h"
+﻿#include "Rule.h"
 #include "CGA.h"
 #include "Shape.h"
 #include <sstream>
@@ -55,12 +55,23 @@ void Rule::apply(Shape* obj, const RuleSet& ruleSet, std::list<Shape*>& stack) c
 	}
 }
 
+/**
+ * 指定されたsizeをsplitする後の、各断片のサイズを計算する。
+ *
+ * @param size							もとのsize
+ * @param sizes							指定された、各断片のサイズ
+ * @param output_names					指定された、各断片の名前
+ * @param ruleSet						ルール (sizeなどで変数が使用されている場合、解決するため)
+ * @param decoded_sizes	[OUT]			計算された、各断片のサイズ
+ * @param decoded_output_names [OUT]	計算された、各断片の名前
+ */
 void Rule::decodeSplitSizes(float size, const std::vector<Value*>& sizes, const std::vector<std::string>& output_names, const RuleSet& ruleSet, std::vector<float>& decoded_sizes, std::vector<std::string>& decoded_output_names) {
 	int regular_count = 0;
 	int floating_count = 0;
 	float regular_sum = 0.0f;
 	float floating_sum = 0.0f;
 	bool repeat = false;
+	int repeat_count = 0;
 
 	for (int i = 0; i < sizes.size(); ++i) {
 		if (sizes[i]->type == Value::TYPE_ABSOLUTE) {
@@ -72,12 +83,14 @@ void Rule::decodeSplitSizes(float size, const std::vector<Value*>& sizes, const 
 		} else if (sizes[i]->type == Value::TYPE_FLOATING) {
 			floating_count++;
 			floating_sum += ruleSet.evalFloat(sizes[i]->value);
+		} else if (sizes[i]->type == Value::TYPE_SET) {
+			repeat_count++;
 		}
 	}
 
-	float scale;
-	if (floating_count > 0) {
-		scale = (size - regular_sum) / floating_sum;
+	float floating_scale = 1.0f;
+	if (floating_count > 0 && repeat_count == 0) {
+		floating_scale = (size - regular_sum) / floating_sum;
 	}
 
 	for (int i = 0; i < sizes.size(); ++i) {
@@ -88,7 +101,7 @@ void Rule::decodeSplitSizes(float size, const std::vector<Value*>& sizes, const 
 			decoded_sizes.push_back(ruleSet.evalFloat(sizes[i]->value) * size);
 			decoded_output_names.push_back(output_names[i]);
 		} else if (sizes[i]->type == Value::TYPE_FLOATING) {
-			decoded_sizes.push_back(ruleSet.evalFloat(sizes[i]->value) * scale);
+			decoded_sizes.push_back(ruleSet.evalFloat(sizes[i]->value) * floating_scale);
 			decoded_output_names.push_back(output_names[i]);
 		} else if (sizes[i]->type == Value::TYPE_SET) {
 			if (sizes[i]->repeat) {
@@ -100,16 +113,16 @@ void Rule::decodeSplitSizes(float size, const std::vector<Value*>& sizes, const 
 				}
 
 				float s = sizes[i]->getEstimateValue(size - regular_sum, ruleSet);
-				int num = (size - regular_sum) / s;
+				int num = (size - regular_sum - floating_sum * floating_scale) / s;
 				for (int k = 0; k < num; ++k) {
-					decodeSplitSizes((size - regular_sum) / num, sizes[i]->values, temp_names, ruleSet, decoded_sizes, decoded_output_names);
+					decodeSplitSizes((size - regular_sum - floating_sum * floating_scale) / num, sizes[i]->values, temp_names, ruleSet, decoded_sizes, decoded_output_names);
 				}
 			} else {
 				std::vector<Value*> temp_ratios(1);
 				temp_ratios[0] = sizes[i];
 				std::vector<std::string> temp_names(1);
 				temp_names[0] = output_names[i];
-				decodeSplitSizes(size - regular_sum, temp_ratios, temp_names, ruleSet, decoded_sizes, decoded_output_names);
+				decodeSplitSizes(size - regular_sum - floating_sum * floating_scale, temp_ratios, temp_names, ruleSet, decoded_sizes, decoded_output_names);
 			}
 		}
 	}
