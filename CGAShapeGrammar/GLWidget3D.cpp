d78c4484493fc9e2a674d8d904e92131b680a53b
gnishida <gnishida@purdue.edu>
2015-12-27 13:06:40 -0500

    update ledge_04.xml

--- CGAShapeGrammar/GLWidget3D.cpp
+++ CGAShapeGrammar/GLWidget3D.cpp
@@ -501,7 +501,7 @@ void GLWidget3D::loadCGA(char* filename) {
 	}
 #endif
 
-#if 0
+#if 1
 	{ // for ledge
 		float object_width = 12.0f;
 		float object_depth = 0.5f;
@@ -580,7 +580,7 @@ void GLWidget3D::loadCGA(char* filename) {
 	}
 #endif
 
-#if 1
+#if 0
 	{ // for building Paris2
 		float object_width = 28.0f;
 		float object_depth = 20.0f;
